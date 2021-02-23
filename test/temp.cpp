888e41b812439a747b22ade780dd339e8005ee8f
melkaj <meldinkaj@gmail.com>
2021-02-22 20:44:48 -0500

    Modifying tests. Added a test to check the GetOccupation method. Still need to test the other methods

--- test/temp.cpp
+++ test/temp.cpp
@@ -3,13 +3,41 @@
 #include "../src/datastructure/datastructure.h"
 #include "../lib/catch2/catch_amalgamated.hpp"
 
-TEST_CASE("Testing the datastructure stuff...")
+TEST_CASE("Initializing tests...")
 {
-    datastructure_std::Datastructure ds;
-    ds.AddElem(100001, "name", "occ");
-
-    // REQUIRE(ds.GetOccupation);
-
     int temp = 0;
     REQUIRE(temp == 0);
 }
+
+TEST_CASE("Testing the Datastructure class...")
+{
+    int sos = 120000;
+
+	string nums[16] = { "1", "2", "3" , "4",
+		"5", "6", "7", "8", "9", "10",
+		"11", "12", "13", "14", "15",
+		"16" };
+
+    datastructure_std::Datastructure ds;
+    
+    // Adding the elements
+	for (int i = 0; i < 16; i++)
+	{
+		ds.AddElem(sos+i, nums[i], nums[i]);
+	}
+
+    SECTION("Getting occupation of certain entries")
+    {
+        REQUIRE(ds.GetOccupation(120000) == "1");
+        REQUIRE(ds.GetOccupation(120004) == "5");
+        REQUIRE(ds.GetOccupation(120007) == "8");
+        REQUIRE(ds.GetOccupation(120023) == "Empty");
+    }
+
+}
+
+
+
+
+
+
